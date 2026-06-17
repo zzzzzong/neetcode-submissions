995bafd6cf51bd7d67a07c5c744234920228ef0b
Caius Yang <164485400+zzzzzong@users.noreply.github.com>
2026-06-17 17:30:48 +0800

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # brute force
+        n = len(temperatures)
+        res = []
+
+        for left in range(n):
+            for right in range(left + 1, n):
+                if temperatures[left] < temperatures[right]:
+                    res.append(right - left)
+                    break
+                
+                elif right == n - 1:
+                    res.append(0)
+                else:
+                    pass
+        
+        res.append(0)
+        
+        return res
