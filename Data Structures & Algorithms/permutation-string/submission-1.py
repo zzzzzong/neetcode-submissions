16a4d51e955edcebf9bf23ae7399dfaf01e65cac
Caius Yang <164485400+zzzzzong@users.noreply.github.com>
2026-07-01 15:45:41 +0800

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,26 @@
+from collections import Counter
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # pruning
+        if len(s1) > len(s2): return False
+        if s1 == s2: return True
+        
+        n = len(s1)
+        m = len(s2)
+
+        if n == 1: return s1 in s2
+        
+        # main logic
+
+        # time: O((m - n) * n), space: O(k), where k is number of alphabet
+        c1 = Counter(s1)
+
+        for left in range(m - n + 1):
+            if s2[left] in c1:
+                if Counter(s2[left: left + n]) == c1:
+                    return True
+            else:
+                continue
+        
+        return False
