519fe5552ffdb2028ee112d22f17ecd9e07479e8
Caius Yang <164485400+zzzzzong@users.noreply.github.com>
2026-07-09 23:48:37 +0800

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.go
@@ -0,0 +1,10 @@
+func hasDuplicate(nums []int) bool {
+    for i := 0; i < len(nums); i++ {
+        for j := i + 1; j < len(nums); j ++ {
+            if nums[i] == nums[j]{
+                return true
+            }
+        } 
+    }
+    return false
+}
