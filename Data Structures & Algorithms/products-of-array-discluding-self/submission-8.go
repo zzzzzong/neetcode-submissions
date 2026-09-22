7d37478308ccca257465317bfe446bec8a5ab7aa
Caius Yang <164485400+zzzzzong@users.noreply.github.com>
2026-09-22 17:22:34 +0800

    Add: products-of-array-discluding-self - submission-8

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-8.go
@@ -0,0 +1,19 @@
+func productExceptSelf(nums []int) []int {
+    n := len(nums)
+    res := make([]int, n)
+
+    prefix := 1
+    for i := 0; i < n; i++ {
+        res[i] = prefix
+        prefix *= nums[i]
+    }
+
+
+    postfix := 1
+    for i := n - 1; i >= 0; i-- {
+        res[i] *= postfix
+        postfix *= nums[i]
+    }
+
+    return res
+}
