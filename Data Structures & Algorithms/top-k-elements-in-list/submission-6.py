a4ec5e2cce47059adb784d2e5b37d8e37b679bf9
Caius Yang <164485400+zzzzzong@users.noreply.github.com>
2026-09-22 15:50:45 +0800

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.py
@@ -0,0 +1,23 @@
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # heap x hashmap, time: O(nlogk), space: O(n)
+        hmap = {}
+
+        for char in nums:
+            if char not in hmap:
+                hmap[char] = 1
+                continue
+            hmap[char] += 1
+
+        min_heap = []
+        
+        for key in hmap:
+            heapq.heappush(min_heap, (hmap[key], key))
+            if len(min_heap) == k + 1:
+                heapq.heappop(min_heap)
+        
+        ans = [i[1] for i in min_heap]
+
+        return ans
